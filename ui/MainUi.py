bd7ebd6c39cfb65f0542a6615afe9fb99e0c1036
Tabyretka <opqwerty20@yandex.ru>
2022-06-13 20:55:47 +0300

    initial commit

--- ui/MainUi.py
+++ ui/MainUi.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file '.\archive\ui.ui'
+# Form implementation generated from reading ui file '.\archive\MainUi.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.4
 #
